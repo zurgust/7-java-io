f776f6ab9140bdac9d6752b89d1ff146c8b99fca
Vladimir Polyakov <vladimir.p.polyakov@gmail.com>
2019-04-13 00:24:10 +0500

    Update Task04Main.java

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,6 @@
 package com.example.task04;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
@@ -10,16 +9,6 @@ public class Task04Main {
         // - запустить программу
         // - проверить, что получилось 351.731900
 
-        Scanner scanner = new Scanner(System.in);
-        double x = 0;
-        while (scanner.hasNext()) {
-            String next = scanner.next();
-            try {
-                x += Double.parseDouble(next);
-            } catch (Exception e) {
-                //ignore
-            }
-        }
-        System.out.println(String.format("%.6f", x));
+        System.out.println("0.0");
     }
 }
