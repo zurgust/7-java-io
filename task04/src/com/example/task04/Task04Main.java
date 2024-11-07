8b53514bcdf5ad7bd5feaef8880ab09bb4ca5db4
Zakharov Danila <danila.zaxarov.2015@gmail.com>
2024-11-08 01:38:15 +0300

    task 04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,29 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+        Locale.setDefault(Locale.US);
 
-        System.out.println("0.0");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        double sum = 0.0;
+
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            String[] tokens = line.split("\\s+");
+            for (String token : tokens) {
+                try {
+                    double number = Double.parseDouble(token);
+                    sum += number;
+                } catch (NumberFormatException e) {
+                }
+            }
+        }
+
+        System.out.printf("%.6f%n", sum);
     }
 }
