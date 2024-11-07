73c66b27459c60358b52967e52b32b378cd3b674
Zakharov Danila <danila.zaxarov.2015@gmail.com>
2024-11-07 23:10:39 +0300

    task 02

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,28 +1,22 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        int checksum = 0; // Начальная контрольная сумма равна нулю
+        int checksum = 0; // нач контрольная сумма = 0
         int currentByte;
 
         if (inputStream == null) {
             throw new IllegalArgumentException();
         }
 
-        // Читаем байты из входного потока
         while ((currentByte = inputStream.read()) != -1) {
             checksum = Integer.rotateLeft(checksum, 1) ^ currentByte;
         }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,34 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int previousByte = -1;
+        int currentByte;
+
+        while ((currentByte = System.in.read()) != -1) {
+            if (previousByte == 13) {
+                if (currentByte == 10) {
+                    System.out.write(10);
+                    previousByte = -1;
+                } else if (currentByte == 13) {
+                    System.out.write(13);
+                } else {
+                    System.out.write(13);
+                    System.out.write(currentByte);
+                    previousByte = -1;
+                }
+            } else {
+                if (currentByte == 13) {
+                    previousByte = 13;
+                } else {
+                    System.out.write(currentByte);
+                }
+            }
+        }
+
+        if (previousByte == 13) {
+            System.out.write(13);
+        }
+
+        System.out.flush();
     }
 }
